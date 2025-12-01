ce3c51c07184e00236ae90012117b9604025dc89
leeglb <galbraithlee76@gmail.com>
2025-12-01 15:05:58 +1000

    stuff

--- Alarm-System.py
+++ Alarm-System.py
@@ -1,5 +1,33 @@
-import cv2  
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+import cv2
 
+from ultralytics import solutions
+
+cap = cv2.VideoCapture(0)
+assert cap.isOpened(), "Error reading video file"
+
+# Video writer
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+video_writer = cv2.VideoWriter("security_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+
+# Process video
+while cap.isOpened():
+    success, im0 = cap.read()
+
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+
+
+    # print(results)  # access the output
+
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('q'):
+        
+        cap.release()
+        cv2.destroyAllWindows()  # destroy all opened windows
+            
+        break
+        
---
+++ README.txt
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+Project 1: Rock Paper Scissors
+
+-- Packages Installed -- 
+1. Mediapipe
+2. OpenCv
+3. Random
+4. Time
+5. Datetime 
+
+Project 2: Alarm-System
+
+-- Packages Installed -- 
+1. Mediapipe 
+2. OpenCv
+3. Ultralytics - YOLOV8
